f5cd88094e092b8586c2f95e6ac81f783b078af9
Thapelo Mokotelakoena <thapeloghothini@gmail.com>
2024-04-25 22:36:44 +0200

    Added slider custom ad page

--- src/app/components/ad/ad.component.ts
+++ src/app/components/ad/ad.component.ts
@@ -9,21 +9,25 @@ import Typed from 'typed.js';
   styleUrls: ['./ad.component.scss']
 })
 export class AdComponent implements OnInit {
-
+  isLeftDisabled: boolean = true;
+  isRightDisabled: boolean = false;
   contactNumber: any = '072 714 8449';
-
+  selectedPic: number = 0;
+  adImages: any[] = ['https://photos.webuycars.co.za/photobooth/6B9414091/Images/6B94140910.webp',
+    'https://photos.webuycars.co.za/photobooth/6B9414091/Images/6B941409112.webp','https://photos.webuycars.co.za/photobooth/6B9414091/Images/6B94140915.webp'
+  ]
   constructor(private dialogRef: MatDialogRef<AdComponent>, private snackbar: MatSnackBar) {
   }
 
   ngOnInit(): void {
-    const appTitleElement = document.getElementById('auto-type') as HTMLElement;
-    var typed = new Typed(appTitleElement, {
-      strings: [this.contactNumber],
-      typeSpeed: 150,
-      backSpeed: 150,
-      loop: true,
-      showCursor: false
-    })
+    // const appTitleElement = document.getElementById('auto-type') as HTMLElement;
+    // var typed = new Typed(appTitleElement, {
+    //   strings: [this.contactNumber],
+    //   typeSpeed: 150,
+    //   backSpeed: 150,
+    //   loop: true,
+    //   showCursor: false
+    // })
   }
 
   setCookie(cName: any, cValue: any, cExpDate: any) {
@@ -47,6 +51,22 @@ export class AdComponent implements OnInit {
         navigator.clipboard.writeText(this.contactNumber);
         this.snackbar.open('Copied!', 'OK', { duration: 3000 });
         break;
+    }
+  }
+
+  handleOnPicture(position: any) {
+    if(position === 'right') {
+      this.selectedPic++;
+      this.isLeftDisabled = false;
+      if(this.selectedPic >= this.adImages.length - 1) {
+        this.isRightDisabled = true;
+      }
+    } else {
+      this.selectedPic--;
+      this.isRightDisabled = false;
+      if(this.selectedPic === 0) {
+        this.isLeftDisabled = true;
+      }
     }
   }
 
